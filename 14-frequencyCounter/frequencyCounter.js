355ccacf77535295aa3ab1115ff952a361f78b87
ArslanM786 <arslanmahmood321@gmail.com>
2020-06-03 02:34:25 -0400

    Solved code

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,18 @@
 function frequencyCounter(word) {
   // Insert code here;
+ let frequency = {};
+
+  for (var i = 0; i < word.length; i++) {
+      let char = word.charAt(i);
+
+      if (frequency[char]) {
+         frequency[char]++;
+      } else {
+         frequency[char] = 1;
+      }
+  }
+
+  return frequency;
 }
 
 // Do not edit this line;
