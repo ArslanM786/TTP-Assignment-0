bf9e89aeae9ecf4bf47871ed2aec3714ada3aec4
ArslanM786 <arslanmahmood321@gmail.com>
2020-06-03 00:24:43 -0400

    Solved code

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,16 @@
 function titleCaseEdit(title) {
   // Insert code here;
+let arr = title.split(" ");
+
+//let result="";
+
+for (let i = 0; i < arr.length; i += 1) {
+ arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+// result+=arr[i] + " "; 
+}
+//result = arr.toString(); 
+return arr.join(" ");
+
 }
 
 // Do not edit this line;
