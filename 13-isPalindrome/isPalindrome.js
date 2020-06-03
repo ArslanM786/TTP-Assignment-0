bb81f76894729f93ef4c81778b976376c266c1b0
ArslanM786 <arslanmahmood321@gmail.com>
2020-06-03 02:21:48 -0400

    Solved code

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
   // Insert code here;
+ for(let i = 0; i < word.length; i++){
+    if(word.charAt(i) != word.charAt(word.length - 1 - i)){
+      return false;
+    }
+  }
+
+  return true;
 }
+
 
 // Do not edit this line;
 module.exports = isPalindrome;
