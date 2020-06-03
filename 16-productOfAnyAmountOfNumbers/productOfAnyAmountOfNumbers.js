4c6ade574c75a824706f6712f7d40d6019a2299c
ArslanM786 <arslanmahmood321@gmail.com>
2020-06-03 02:43:58 -0400

    Solved code

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+let prod = 1;
+    for (let i = 0; i < arguments.length; i++) {
+        prod = prod*arguments[i];
+    }
+    return prod;
 }
 
 // Do not edit this line;
