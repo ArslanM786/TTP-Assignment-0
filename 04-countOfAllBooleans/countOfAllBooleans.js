cdf26fe2f21744675e801a040376b868f65e44b7
ArslanM786 <arslanmahmood321@gmail.com>
2020-06-03 00:52:36 -0400

    Solved code

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+let counter =0;
+for(let i=0; i < arr.length; i+=1){
+	if((typeof arr[i]) == 'boolean')
+		counter = counter + 1;
+}
+return counter;
+
 }
 
 // Do not edit this line;
