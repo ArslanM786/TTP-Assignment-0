6d27c350b0a3181ea36f0ba17ea9df050a4979a6
ArslanM786 <arslanmahmood321@gmail.com>
2020-06-03 02:45:29 -0400

    Solved code

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+let set = new Set();
+  for (let i = 0; i < args.length; i++) {
+    for (let element of args[i]) {
+      set.add(element); 
+    }
+  }
+  return set;
 }
 
 // Do not edit this line;
